4e6ebf4be8ce4745b6c48a8048b6fe3043e8b0e3
Peter Hilden <peter.e.hilden@gmail.com>
2021-01-23 17:35:56 +0200

    changed response of failed org auth

--- utils/middleware.js
+++ utils/middleware.js
@@ -2,14 +2,18 @@ const jwt = require('jsonwebtoken')
 
 const organizationAuth = (request, response, next) => {
   const token = request.cookies['jwt']
+  try {
+    const user = jwt.verify(token, process.env.SECRET)
 
-  const user = jwt.verify(token, process.env.SECRET)
-
-  if (user.type === 'organization') {
-    next()
-  } else if (user.type === 'employee') {
-    return response.status(403).json('You do not have required permission')
-  } else {
+    if (user.type === 'organization') {
+      next()
+    } else if (user.type === 'employee') {
+      return response.status(403).json('You do not have required permission')
+    } else {
+      return response.status(401).json('Token missing or invalid')
+    }
+  } catch (exception) {
+    console.log(exception)
     return response.status(401).json('Token missing or invalid')
   }
 }
