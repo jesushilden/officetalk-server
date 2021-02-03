920aa41797436f523eeb0770f13be9503cf55d05
Peter Hilden <peter.e.hilden@gmail.com>
2021-02-03 13:36:46 +0200

    fixed employee socket emit

--- index.js
+++ index.js
@@ -29,12 +29,43 @@ const clients = {}
 
 workspaces.on('connection', async (socket) => {
   const workspace = socket.nsp
-  console.log('client', socket.id, 'connected to workspace', workspace.name)
-  const employees = Array.from(workspace.sockets.keys()).map(socketId => clients[socketId])
+
+  if (!clients[workspace.name]) {
+    clients[workspace.name] = {}
+  }
+
+  if (!clients[workspace.name][socket.id]) {
+    clients[workspace.name][socket.id] = {
+      userId: socket.user._id,
+      muted: false,
+      silenced: false,
+      position: {
+        room: -1,
+        coordinates: {
+          x: 5,
+          y: 360
+        }
+      }
+    }
+  }
+
+  const employees = Object.keys(clients[workspace.name])
+    .map(socketId => clients[workspace.name][socketId])
+    .filter((user, index, array) => array.indexOf(user) === index)
+
   workspace.emit('employees', employees)
 
+  console.log('client', socket.id, 'connected to workspace', workspace.name)
+
   socket.on('disconnect', () => {
-    delete clients[socket.id]
+    delete clients[workspace.name][socket.id]
+
+    const employees = Object.keys(clients[workspace.name])
+      .map(socketId => clients[workspace.name][socketId])
+      .filter((user, index, array) => array.indexOf(user) === index)
+
+    workspace.emit('employees', employees)
+
     console.log('user', socket.id, 'disconnected')
   })
 })
@@ -43,9 +74,7 @@ workspaces.use(async (socket, next) => {
   // ensure the user has access to the workspace
   const user = await signinService.signin(socket.handshake.auth.token)
   if (user.organization.toString() === socket.nsp.name.substring(1)) {
-    if (!clients[socket.id]) {
-      clients[socket.id] = user
-    }
+    socket.user = user
     next()
   } else {
     next(new Error('Token not valid'))
