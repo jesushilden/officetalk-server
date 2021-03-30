21caefc5090e7705ddd32ab75d873547ab51c921
Peter Hilden <peter.e.hilden@gmail.com>
2021-03-30 13:40:15 +0300

    added turn servers

--- officetalkSocket.js
+++ officetalkSocket.js
@@ -141,7 +141,7 @@ class OfficetalkSocket {
   async emitInitialDataToSocket(socketId, organizationId) {
     const statesToEmit = this.employeeStates.filter(state => state.organizationId === organizationId)
     const twilioToken = await this.twilio.tokens.create()
-    const iceServers = twilioToken.iceServers.filter(server => server.url.includes('stun'))
+    const iceServers = twilioToken.iceServers
     this.io.to(socketId).emit('initialData', { employeeStates: statesToEmit, iceServers })
   }
 
